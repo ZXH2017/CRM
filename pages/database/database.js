639165789ac59babe99a1b70e77bc86db6af3cd9
ZXH2017 <564407380@qq.com>
2018-03-01 15:33:10 +0800

    解决项目列表数据渲染不出来的bug

--- pages/database/database.js
+++ pages/database/database.js
@@ -10,115 +10,7 @@ Page({
   data: {
     modules: [{ id: '1', name: '项目' }, { id: '2', name: '业主客户' }, { id: '3', name: '企业客户' }, { id: '4', name: '站点' }, { id: '5', name: '我的文档' }],
     currentItem: "1",
-    proList: {
-      "code": 200,
-      "data": [
-        {
-          "letter": "A",
-          "data": [
-            {
-              "id": 154,
-              "token": "52gSttT1",
-              "project_name": "aaxxn",
-              "project_type": "基站",
-              "business_type": "软件类",
-              "data_source": "业务员上传",
-              "project_collect_time": "2018-01-23 11:02:00",
-              "founder": "xxnjq",
-              "initials": "A"
-            },
-            {
-              "id": 162,
-              "token": "lq7thhEO",
-              "project_name": "a测试图片",
-              "project_type": "驻地网",
-              "business_type": "软件类",
-              "data_source": "业务员上传",
-              "project_collect_time": "2018-02-01 02:05:00",
-              "founder": "gvc",
-              "initials": "A"
-            },
-            {
-              "id": 173,
-              "token": "SGDoc2!p",
-              "project_name": "A_ZXH2017",
-              "project_type": "基站",
-              "business_type": "通讯类",
-              "data_source": "业务员上传",
-              "project_collect_time": "2018-02-01 09:33:00",
-              "founder": "SDAS ",
-              "initials": "A"
-            },
-            {
-              "id": 174,
-              "token": "!lgH52@I",
-              "project_name": "AZ_a_small_insect",
-              "project_type": "基站",
-              "business_type": "软件类",
-              "data_source": "业主上传",
-              "project_collect_time": "2018-02-01 09:34:00",
-              "founder": "ASDA",
-              "initials": "A"
-            }
-          ]
-        },
-        {
-          "letter": "K",
-          "data": [
-            {
-              "id": 157,
-              "token": "UHNtan9W",
-              "project_name": "可口可乐了看看",
-              "project_type": "驻地网",
-              "business_type": "软件类",
-              "data_source": "业主上传",
-              "project_collect_time": "2018-01-24 02:39:00",
-              "founder": "兔兔",
-              "initials": "K"
-            }
-          ]
-        },
-        {
-          "letter": "X",
-          "data": [
-            {
-              "id": 81,
-              "token": "J7C!$7D1",
-              "project_name": "项目名称",
-              "project_type": "项目类型",
-              "business_type": "业务类型",
-              "data_source": "数据来源",
-              "project_collect_time": "0000-00-00 00:00:00",
-              "founder": "创建人",
-              "initials": "X"
-            },
-            {
-              "id": 82,
-              "token": "OZgi96*0",
-              "project_name": "项目名称",
-              "project_type": "项目类型",
-              "business_type": "业务类型",
-              "data_source": "数据来源",
-              "project_collect_time": "0000-00-00 00:00:00",
-              "founder": "创建人",
-              "initials": "X"
-            },
-            {
-              "id": 86,
-              "token": "2NjYV1Z8",
-              "project_name": "项目名称",
-              "project_type": "项目类型",
-              "business_type": "业务类型",
-              "data_source": "数据来源",
-              "project_collect_time": "0000-00-00 00:00:00",
-              "founder": "创建人",
-              "initials": "X"
-            }
-          ]
-        }
-      ],
-      "msg": "成功"
-    },
+    proList: {},
   },
   // 点击导航选择查看对应模块列表
   partClick: function (options) {
@@ -139,11 +31,10 @@ Page({
       },
       success: function (res) {
         if (res.data.code == 200) {
-          that.setData = ({
+          that.setData({
             proList: res.data
           })
         }
-        console.log(res.data);
       },
       fail: function (res) {
         console.log(res)
