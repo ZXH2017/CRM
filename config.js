43f55dafd90aeea53b3eb36692171d3ffcbb29c3
ZXH2017 <564407380@qq.com>
2018-03-01 17:39:14 +0800

    新增五个列表的数据

--- config.js
+++ config.js
@@ -5,5 +5,17 @@ var config = {
   host,
   //获取项目列表
   project_list: `${host}/customer/index/appProjectList`,
+
+  //获取业主客户列表
+  owner_list: `${host}/customer/index/appOwnerList`,
+
+  //获取企业客户列表
+  company_list: `${host}/customer/index/appBidCompanyList`,
+
+  //获取站点列表
+  site_list: `${host}/customer/index/appSiteList`,
+  
+  //获取我的文档列表
+  file_list: `${host}/customer/index/appMyDocument`,
 };
 module.exports = config;
--- pages/database/database.js
+++ pages/database/database.js
@@ -1,6 +1,9 @@
 // pages/database/database.js
 const project_list = require('../../config').project_list;
-console.log(project_list)
+const owner_list = require('../../config').owner_list;
+const company_list = require('../../config').company_list;
+const site_list = require('../../config').site_list;
+const file_list = require('../../config').file_list;
 
 Page({
 
@@ -11,16 +14,27 @@ Page({
     modules: [{ id: '1', name: '项目' }, { id: '2', name: '业主客户' }, { id: '3', name: '企业客户' }, { id: '4', name: '站点' }, { id: '5', name: '我的文档' }],
     currentItem: "1",
     proList: {},
+    show: [true],
   },
   // 点击导航选择查看对应模块列表
   partClick: function (options) {
     var that = this;
     var id = options.currentTarget.dataset.id;
+    var show = [];
+
+    for (var i = 0; i < 5; i++) {
+      if (i == (id - 1)) {
+        show[i] = true;
+      } else {
+        show[i] = false;
+      }
+    }
     that.setData({
-      'currentItem': id
+      'currentItem': id,
+      show: show
     })
   },
-
+  //获取项目列表
   getProjectListMethod: function () {
     var that = this;
     wx.request({
@@ -41,12 +55,102 @@ Page({
       }
     })
   },
+  //获取业主客户列表
+  getOwnerListMethod: function () {
+    var that = this;
+    wx.request({
+      url: owner_list,
+      method: "GET",
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        if (res.data.code == 200) {
+          that.setData({
+            ownerList: res.data
+          })
+        }
+      },
+      fail: function (res) {
+        console.log(res)
+      }
+    })
+  },
+  //获取企业客户列表
+  getCompanyListMethod: function () {
+    var that = this;
+    wx.request({
+      url: company_list,
+      method: "GET",
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        if (res.data.code == 200) {
+          that.setData({
+            companyList: res.data
+          })
+        }
+      },
+      fail: function (res) {
+        console.log(res)
+      }
+    })
+  },
+  //获取站点列表
+  getSiteListMethod: function () {
+    var that = this;
+    wx.request({
+      url: site_list,
+      method: "GET",
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        if (res.data.code == 200) {
+          that.setData({
+            siteList: res.data
+          })
+        }
+      },
+      fail: function (res) {
+        console.log(res)
+      }
+    })
+  },
+  //获取站点列表
+  getFileListMethod: function () {
+    var that = this;
+    wx.request({
+      url: file_list,
+      method: "GET",
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      data: { user: 'admin' },
+      success: function (res) {
+        if (res.data.code == 200) {
+          that.setData({
+            fileList: res.data
+          })
+          console.log(res)
+        }
+      },
+      fail: function (res) {
+        console.log(res)
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     var that = this;
-    that.getProjectListMethod();
+    that.getProjectListMethod();//获取项目列表
+    that.getOwnerListMethod();
+    that.getCompanyListMethod();
+    that.getSiteListMethod();
+    that.getFileListMethod();
   },
 
   /**
