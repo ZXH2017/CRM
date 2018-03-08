82454f3b75f57db14998633892df869b6fd6a540
ZXH2017 <564407380@qq.com>
2018-03-08 14:23:04 +0800

    ..

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,15 @@
 // pages/index/index.js
+var app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+
   },
-  goDatabase:function(e){
+  goDatabase: function (e) {
     wx.redirectTo({
       url: '../database/database',
     })
@@ -16,55 +18,65 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that = this
+    //res = app.getUserInfo()
+    app.getUserInfo(function (res) {
+      console.log('app.getUserInfo', res)
+      // that.setData({
+      //   nickName: res.nickName,
+      //   avatarUrl: res.avatarUrl
+      // })
+     
+    })
+    console.log(app.globalData)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
